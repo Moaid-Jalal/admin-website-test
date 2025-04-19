c0a05b41f7ac7d8a46d794a54a85e08834d903fc
moaid <moaidjalal2007@gmail.com>
2025-04-19 05:31:22 -0700

    sadsa

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from '@/components/navbar';
-import Footer from '@/components/footer';
 import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({ subsets: ['latin'] });
