c0ed218ef42f4b6a0e0c36e0f46affe3062da4e7
moaid <moaidjalal2007@gmail.com>
2025-04-19 06:07:42 -0700

    sdasdas

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,12 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    <div className="relative">
-      
+    <div className="relative flex items-center justify-center min-h-screen">
+      <Link href="/login">
+        <Button>
+          Login <ArrowRight className="ml-2 h-4 w-4" />
+        </Button>
+      </Link>
     </div>
   )
 }
