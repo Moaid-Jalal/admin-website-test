11dad9cb72ed90f7f9c03b2ba7684ab7c1463c2d
moaid <moaidjalal2007@gmail.com>
2025-04-19 05:24:51 -0700

    sadas

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,6 @@ export default function RootLayout({
               {children}
               <Toaster />
             </main>
-            {/* <Footer /> */}
           </div>
         </ThemeProvider>
       </body>
