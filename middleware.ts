f1b1f5becf0c39e86b8a796a8d957253a397f91e
moaid <moaidjalal2007@gmail.com>
2025-04-21 08:25:13 -0700

    oasda

--- middleware.ts
+++ middleware.ts
@@ -4,16 +4,17 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value;
   const { pathname } = request.nextUrl;
 
-  if ((pathname === '/login' || pathname === '/') && token) {
-    const url = request.nextUrl.clone();
-    url.pathname = '/admin';
-    return NextResponse.redirect(url);
-  }
-
-  
+  // أولاً التحقق من الوصول إلى الصفحات المحمية
   if (pathname.startsWith('/admin') && !token) {
     const url = request.nextUrl.clone();
     url.pathname = '/login';
+    return NextResponse.redirect(url);
+  }
+
+  // إذا كان التوكن موجودًا وأنت في صفحة login أو الصفحة الرئيسية، يتم تحويلك إلى /admin
+  if ((pathname === '/login' || pathname === '/') && token) {
+    const url = request.nextUrl.clone();
+    url.pathname = '/admin';
     return NextResponse.redirect(url);
   }
 
